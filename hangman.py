cac6e7ac6d58231d8f45e47f3421996dbc090a55
ANDRAS VARGA <vargaae@hotmail.com>
2025-03-20 15:25:07 +0100

    implement validation for type to input more than 1 letter

--- hangman.py
+++ hangman.py
@@ -1,19 +1,19 @@
 import random
-def get_rejtveny(feladvany, talalatok):
-    return ''.join(c if c in talalatok else '_' for c in feladvany)
-def getFeladvany():
+def get_solution(feladvany, talalatok):
+    return ''.join(chr if chr in talalatok else '_' for chr in feladvany)
+def run_hangman():
     feladvany = random.choice(["macska", "kutya", "cica", "egér"]).upper()
     talalatok, hibapontok = set(), 0
     
     while hibapontok < 13:
         tipp = input("Mi a tipped?").upper()
-        if not tipp or not tipp.isalpha():
+        if not tipp or len(tipp) > 1 or not tipp.isalpha():
             print("Próbáld újra")
             continue
         
         if tipp in feladvany:
             talalatok.add(tipp)
-            rejtveny = get_rejtveny(feladvany, talalatok)
+            rejtveny = get_solution(feladvany, talalatok)
             print(f'Jó tipp!\n{rejtveny}')
             if rejtveny == feladvany:
                 print("Győztél!")
@@ -21,4 +21,4 @@ def getFeladvany():
         else:
             hibapontok += 1
             print(f'Rossz tipp - Hibapontok: {hibapontok}')
-getFeladvany()
+run_hangman()
