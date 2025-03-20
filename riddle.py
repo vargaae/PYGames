378b57f479dfd7b034ef1872f885574e2523a474
ANDRAS VARGA <vargaae@hotmail.com>
2025-03-20 14:22:29 +0100

    first commit

---
+++ riddle.py
@@ -0,0 +1,24 @@
+import random
+def get_rejtveny(feladvany, talalatok):
+    return ''.join(c if c in talalatok else '_' for c in feladvany)
+def getFeladvany():
+    feladvany = random.choice(["macska", "kutya", "cica", "egér"]).upper()
+    talalatok, hibapontok = set(), 0
+    
+    while hibapontok < 13:
+        tipp = input("Mi a tipped?").upper()
+        if not tipp:
+            print("Próbáld újra")
+            continue
+        
+        if tipp in feladvany:
+            talalatok.add(tipp)
+            rejtveny = get_rejtveny(feladvany, talalatok)
+            print(f'Jó tipp! {rejtveny}')
+            if rejtveny == feladvany:
+                print("Győztél!")
+                return
+        else:
+            hibapontok += 1
+            print(f'Rossz tipp - Hibapontok: {hibapontok}')
+getFeladvany()
